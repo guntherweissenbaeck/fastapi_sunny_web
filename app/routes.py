f67c10d91548e3e7398b0023beac20ee543d010f
gw3000 <weissenbaeck@posteo.de>
2023-05-11 17:12:35 +0200

    added more routes

--- app/routes.py
+++ app/routes.py
@@ -7,18 +7,46 @@ from .models import SunnyWebData
 router = APIRouter()
 
 
+@router.get("/power/{year}")
+async def retrieve_power_per_year(year: int):
+    cursor.execute(
+        """SELECT created_at, power FROM t_power WHERE power <> 0 AND EXTRACT(
+        YEAR FROM CREATED_AT) = %s""",
+        (year,))
+    power = cursor.fetchall()
+    if not power:
+        return {"message": "No Data within the given Date"}
+    return {"data": power}
+
+
 @router.get("/power/{year}/{month}")
-async def retrieve_power_per_moth(year: int, month: int):
+async def retrieve_power_per_month(year: int, month: int):
     cursor.execute(
         """SELECT created_at, power FROM t_power WHERE power <> 0 AND EXTRACT(
         YEAR FROM CREATED_AT) = %s AND EXTRACT( MONTH FROM CREATED_AT) = %s""",
         (year, month))
     power = cursor.fetchall()
+    if not power:
+        return {"message": "No Data within the given Date"}
+    return {"data": power}
+
+
+@router.get("/power/{year}/{month}/{day}")
+async def retrieve_power_per_day(year: int, month: int, day: int):
+    cursor.execute(
+        """SELECT created_at, power FROM t_power WHERE power <> 0 AND EXTRACT(
+        YEAR FROM CREATED_AT) = %s AND EXTRACT( MONTH FROM CREATED_AT) = %s
+        AND EXTRACT( DAY FROM CREATED_AT) = %s
+        """,
+        (year, month, day))
+    power = cursor.fetchall()
+    if not power:
+        return {"message": "No Data within the given Date"}
     return {"data": power}
 
 
 @router.get("/d-yield/{year}")
-async def retrieve_daily_yield_per_moth(year: int):
+async def retrieve_daily_yield_per_year(year: int):
     cursor.execute(
         """SELECT created_at::timestamp::date, daily_yield FROM t_power WHERE
         EXTRACT( YEAR FROM CREATED_AT) = %s AND EXTRACT(HOUR FROM CREATED_AT)
@@ -26,11 +54,13 @@ async def retrieve_daily_yield_per_moth(year: int):
         ASC""",
         (year,))
     dyield = cursor.fetchall()
+    if not dyield:
+        return {"message": "No Data within the given Date"}
     return {"data": dyield}
 
 
 @router.get("/d-yield/{year}/{month}")
-async def retrieve_daily_yield_per_moth(year: int, month: int):
+async def retrieve_daily_yield_per_month(year: int, month: int):
     cursor.execute(
         """SELECT created_at::timestamp::date, daily_yield FROM t_power WHERE
         daily_yield <> 0 AND EXTRACT( YEAR FROM CREATED_AT) = %s AND EXTRACT(
@@ -38,6 +68,23 @@ async def retrieve_daily_yield_per_moth(year: int, month: int):
         AND EXTRACT(MINUTE FROM CREATED_AT) >= 59 ORDER BY created_at ASC""",
         (year, month))
     dyield = cursor.fetchall()
+    if not dyield:
+        return {"message": "No Data within the given Date"}
+    return {"data": dyield}
+
+
+@router.get("/d-yield/{year}/{month}/{day}")
+async def retrieve_daily_yield_per_day(year: int, month: int, day: int):
+    cursor.execute(
+        """SELECT created_at::timestamp::date, daily_yield FROM t_power WHERE
+        daily_yield <> 0 AND EXTRACT( YEAR FROM CREATED_AT) = %s AND EXTRACT(
+            MONTH FROM CREATED_AT) = %s AND EXTRACT( DAY FROM CREATED_AT) = %s
+        AND EXTRACT(HOUR FROM CREATED_AT) >= 23 AND EXTRACT(MINUTE FROM
+        CREATED_AT) >= 59 ORDER BY created_at ASC""",
+        (year, month, day))
+    dyield = cursor.fetchall()
+    if not dyield:
+        return {"message": "No Data within the given Date"}
     return {"data": dyield}
 
 
@@ -59,5 +106,4 @@ async def add_data(data: SunnyWebData):
         (data.power, data.daily_yield, data.total_yield),)
     new_data = cursor.fetchone()
     conn.commit()
-    # data_list.append(data)
     return {"data": new_data}
